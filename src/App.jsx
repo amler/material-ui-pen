3169a0ccb29ae1991d5d58d89ab87f07703fbb80
Heather Amler <amler.heather@gmail.com>
2023-12-08 11:53:19 -0500

    Initial todo list setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import RatingDemo from './RatingDemo'
-import FormDemo from './FormDemo'
-import Navbar from './Navbar'
+import { CssBaseline } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        {/* <RatingDemo/> */}
-        <Navbar/>
-        <FormDemo/>
-      </div>
+      <CssBaseline/>
+      <h1>Todos: </h1>
     </>
   )
 }
-
+// const dummyData: [
+//   {id: '1234', text: 'buy paint', completed: false},
+//   {id: '2345', text: 'remove popcorn ceiling', completed: false},
+//   {id: '3456', text: 'Repair threshold', completed: false},
+// ]
 export default App
