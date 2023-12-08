d7fe71246515a7c0f79d493d0c59af615c274363
Heather Amler <amler.heather@gmail.com>
2023-12-08 14:01:16 -0500

    Setting up a todolist

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 import { List } from '@mui/material';
-
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const dummyData = [
   {id: '1234', text: 'buy paint', completed: false},
@@ -10,12 +16,36 @@ const dummyData = [
 
 export default function TodoList () {
   const [todos, setTodos] = useState(dummyData);
-
+  
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {/* {.map((value) => {
-        
-      } */}
+      {todos.map((todo) => {
+        // const labelId = `checkbox-list-label-${value}`;
+          return (
+            <ListItem
+              key={todo.id}
+              secondaryAction={
+                <IconButton edge="end" aria-label="comments">
+                  <DeleteIcon />
+                </IconButton>
+              }
+              disablePadding
+            >
+              <ListItemButton role={undefined} dense>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={todo.completed}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': todo.id }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={todo.id} primary={todo.text} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
     </List>
   );
 }
